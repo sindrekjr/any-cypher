0249694591870ab32a28d95be5af863392ff7e5c
Sindre S. Kjær <sindre@sindrekjr.no>
2019-11-26 15:54:54 +0100

    Continously parse input and added selection fields

--- js/script.js
+++ js/script.js
@@ -1,26 +1,91 @@
-let cypher, ignoreBracket; 
+let ignoreBracket = "[]"; 
+let selected, cypher; 
 
 $().ready(() => {
-    cypher = cyphers[Object.keys(cyphers)[0]];
-    ignoreBracket = "[]"; 
-
-    $('#input').on('keyup', (event) => {
-        $('#output').val(translate(event.currentTarget.value, cypher));
-    });
-
-    $('#output').on('keyup', (event) => {
-        $('#input').val(translate(event.currentTarget.value, flipObjectValues(cypher)));
-    });
-
-    generateAlphabet(cypher);
+    selectCypher(Object.keys(cyphers)[0]); 
+    addInputListeners();
+    initializeNav();
 });
+
+function selectCypher(name) {
+    if(name) {
+        selected = name; 
+        cypher = cyphers[selected];
+        $('#out .title').html(selected);
+    }
+    generateAlphabet(); 
+}
+
+function addInputListeners() {
+    $('#in .input').on('keyup', function() {
+        $('#out .input').val(translate(this.value, cypher));
+    });
+
+    $('#out .input').on('keyup', function() {
+        $('#in .input').val(translate(this.value, flipObjectValues(cypher)));
+    });
+}
+
+function initializeNav() {
+    for(let c in cyphers) {
+        $('nav').append(
+            $('<label class=select>').css({
+                'background-color': '#' + getColourFromString(c)
+            }).append(
+                $('<input type=radio name=cypher value="' + c + '">')
+            ).append(c)
+        );
+    }
+
+    $('input[type=radio][name=cypher]').change(function() {
+        if(selected != this.value) {
+            selectCypher(this.value);
+            updateOpacity(); 
+        }
+    });
+
+    updateOpacity();
+}
+
+function getColourFromString(str) {
+    let hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    
+    let c = (hash & 0x00FFFF).toString(12).toUpperCase();
+    return "00000".substring(0, 6 - c.length) + c;
+}
+
+function updateOpacity() {
+    $('.select').each(function() {
+        let input = $(this).children()[0];
+        if(input.value === selected) input.checked = true; 
+        this.style.opacity = ($(this).children()[0].checked) ? 1.0 : 0.8;
+    });
+}
+
+function generateAlphabet() {
+    let form = $('<form>'); 
+    for(let letter in cypher) {
+        form.append($('<input>').val(letter));
+    }
+    
+    $('.alphabet-section').html(form);
+}
+
+function flipObjectValues(obj) {
+    let flipped = {}
+    for(let key in obj) flipped[obj[key]] = key;
+    return flipped; 
+}
 
 function translate(text, cypher) {
     let ignore = false; 
     let translated = "";
     for(let i = 0; i < text.length; i++) {
         let c = text[i];
-        let upper = c == c.toUpperCase(); 
+        let upper = !$.isNumeric(c) && c == c.toUpperCase(); 
         if(ignoreBracket.includes(c)) ignore = !ignore; 
 
         if(ignore) {
@@ -36,18 +101,8 @@ function translate(text, cypher) {
     return translated; 
 }
 
-function flipObjectValues(obj) {
-    let flipped = {}
-    for(let key in obj) flipped[obj[key]] = key;
-    return flipped; 
-}
-
-function generateAlphabet(cypher) {
-    
-}
-
 const cyphers = {
-    "al bhed": {
+    "Al Bhed": {
         "a":"y",
         "b":"p",
         "c":"l",
@@ -76,6 +131,31 @@ const cyphers = {
         "z":"w"
     },
     "l33t": {
-
+        "a":"4",
+        "b":"8",
+        "c":"(",
+        "d":"|)",
+        "e":"3",
+        "f":"ph",
+        "g":"6",
+        "h":"|-|",
+        "i":"1",
+        "j":"j",
+        "k":"|<",
+        "l":"l",
+        "m":"^^",
+        "n":"|\\|",
+        "o":"0",
+        "p":"P",
+        "q":"kw",
+        "r":"l2",
+        "s":"$",
+        "t":"7",
+        "u":"|_|",
+        "v":"V",
+        "w":"\\/\\/",
+        "x":"><",
+        "y":"'/",
+        "z":"2"
     }
 }
